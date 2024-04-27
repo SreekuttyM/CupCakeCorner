108f4994782fc2abe492520c91084de614112508
Sreekutty Maya <sreekuttystar@gmail.com>
2024-04-27 22:17:23 +0400

    project setup added

---
+++ CupCakeCorner/CupCakeCorner/CupCakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupCakeCornerApp.swift
+//  CupCakeCorner
+//
+//  Created by Sreekutty Maya on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct CupCakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
